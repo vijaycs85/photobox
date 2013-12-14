49181cfb870af9f083ec8f1ab9018badaaa94088
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-12-14 22:17:17 +0400

    Fixes to photobox initialization.

--- photobox.js
+++ photobox.js
@@ -1,11 +1,11 @@
+/*global Drupal: false, jQuery: false */
+/*jslint devel: true, browser: true, maxerr: 50, indent: 2 */
 (function ($) {
+  "use strict";
 
   Drupal.behaviors.photobox = {};
   Drupal.behaviors.photobox.attach = function(context, settings) {
-    /*  $(document).ready() wrapper required to properly initialize Photobox. */
-    $(document).ready(function(){
-      $('a.photobox', context).photobox();
-    });
+    $('a.photobox', context).parent().photobox('a');
   };
 
 })(jQuery);
