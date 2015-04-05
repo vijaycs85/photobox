9d4f86c0308ab2a12cb6cd7ec63dd34a8fe34c7b
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2015-04-05 21:35:11 +0300

    Implemented [#2413263] - added global Photobox settings form

--- photobox.js
+++ photobox.js
@@ -5,6 +5,9 @@
 
   Drupal.behaviors.photobox = {};
   Drupal.behaviors.photobox.attach = function(context, settings) {
+    // Set Photobox settings.
+    $.extend(window._photobox.defaults, Drupal.settings.photobox);
+
     var galleries = [];
     $('a.photobox', context).each(function(index, element) {
       var gallery = $(this).data('photoboxGallery');
